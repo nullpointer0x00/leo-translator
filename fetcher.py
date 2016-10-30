8b395e80c850db47b10316d2da814e308e9cf6dc
Carlton Hanna <carlton@graymatterssoftware.com>
2016-10-30 09:15:17 -0600

    Add a user agent request header

--- fetcher.py
+++ fetcher.py
@@ -3,6 +3,11 @@ from lxml import html
 
 class LeoFetcher:
 
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0',
+    }
+
+
     def __init__(self):
         self.session = requests.session()
         self.baseUrl = "http://dict.leo.org/dictQuery/m-vocab/ende/query.xml"
@@ -10,4 +15,4 @@ class LeoFetcher:
 
     def search_english_word(self, lang, word):
         url = self.baseUrl + self.params.format(lang, word)
-        return self.session.get(url).content
+        return self.session.get(url, headers = self.headers).content
