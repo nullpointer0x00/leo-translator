4f1bf3eed63651449c894d130e7e6d215f1ae3ea
Carlton Hanna <carlton@graymatterssoftware.com>
2016-10-30 09:14:54 -0600

    Add utf8mb4 as charset for connector

--- database.py
+++ database.py
@@ -5,14 +5,14 @@ from pymysql import MySQLError
 
 
 class MySqlDataSouce:
-
     def __init__(self):
         self.config = {
             'user': 'leo',
             'password': 'leopass',
             'host': '127.0.0.1',
-            'port' : 3406,
-            'database': 'Uebersetzung'
+            'port': 3406,
+            'database': 'Uebersetzung',
+            'charset': 'utf8mb4'
         }
         self.cnx = pymysql.connect(**self.config)
 
@@ -25,8 +25,8 @@ class MySqlDataSouce:
 
     def add_search_history(self, searchTerm, lang):
         query = ("INSERT INTO SearchHistory "
-                        "(SearchTerm, Lang) "
-                        "VALUES (%s, %s)")
+                 "(SearchTerm, Lang) "
+                 "VALUES (%s, %s)")
         data = (searchTerm, lang)
         return self.insert_record(query, data)
 
@@ -37,28 +37,28 @@ class MySqlDataSouce:
         data = (status, id)
         return self.insert_record(query, data)
 
-    def add_english_word(self, word, type) :
+    def add_english_word(self, word, type):
         query = ("INSERT INTO EnglishWord "
-                      "(`Word`, `Type`) "
-                      "VALUES (%s, %s)")
+                 "(`Word`, `Type`) "
+                 "VALUES (%s, %s)")
         data = (str(word), type)
         return self.insert_record(query, data)
 
-    def add_deutsch_word(self, word, type, gender) :
+    def add_deutsch_word(self, word, type, gender):
         query = ("INSERT INTO DeutschWord "
-                      "(`Word`, `Type`, `Gender`) "
-                      "VALUES (%s, %s, %s)")
+                 "(`Word`, `Type`, `Gender`) "
+                 "VALUES (%s, %s, %s)")
         data = (str(word), type, gender)
         return self.insert_record(query, data)
 
     def find_english_word(self, word, type):
         cursor = self.cnx.cursor()
-        type ="NOUN"
+        type = "NOUN"
         query = ("SELECT * FROM EnglishWord where `Word` = %s and `Type` = %s")
         data = (str(word), str(type))
         cursor.execute(query, data)
         rows = cursor.fetchall()
-        if len(rows) is not 0 :
+        if len(rows) is not 0:
             return rows[0]
 
     def find_deutsch_word(self, word, type):
@@ -67,19 +67,18 @@ class MySqlDataSouce:
         data = (str(word), str(type))
         cursor.execute(query, data)
         rows = cursor.fetchall()
-        if len(rows) is not 0 :
+        if len(rows) is not 0:
             return rows[0]
 
     def add_english_deutsch_translation(self, englishWordId, deutschWordId):
         query = ("INSERT INTO EnglishDeutschTranslation "
-                            "(`EnglishWordId`, `DeutschWordId`) "
-                            "VALUES (%s, %s)")
+                 "(`EnglishWordId`, `DeutschWordId`) "
+                 "VALUES (%s, %s)")
         data = (englishWordId, deutschWordId)
         return self.insert_record(query, data)
 
-
     def insert_record(self, query, data):
-        try :
+        try:
             cursor = self.cnx.cursor()
             cursor.execute(query, data)
             id = cursor.lastrowid
@@ -88,5 +87,5 @@ class MySqlDataSouce:
         except MySQLError as e:
             print('Got error {!r}, errno is {}'.format(e, e.args[0]))
             return -1
-        except Exception :
+        except Exception:
             return -1
